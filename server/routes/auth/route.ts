5ea4237086cc1e32a0a3fb50cfea0bb0d12f9d11
GWjun <kb865900@gmail.com>
2025-01-09 14:50:52 +0900

    feat: add auth dto, schema

--- server/routes/auth/route.ts
+++ server/routes/auth/route.ts
@@ -1,46 +1,89 @@
 import type { FastifyInstance } from 'fastify'
+import {
+  LoginDto,
+  LoginSchema,
+  RegisterDto,
+  RegisterSchema,
+} from '@dto/auth.ts'
+
 import bcryptjs from 'bcryptjs'
 import prisma from '@utils/prisma.ts'
 import { TokenService } from '@services/auth/token.ts'
 import { getAPIError } from '@utils/getAPIError.ts'
 
-interface LoginBody {
-  email: string
-  password: string
-}
-
-interface RegisterBody {
-  email: string
-  password: string
-  nickname: string
-  language: string
-}
-
 export default async function authRouter(fastify: FastifyInstance) {
   const tokenService = new TokenService(fastify)
 
-  fastify.post<{ Body: RegisterBody }>('/register', async (req, reply) => {
-    const { email, password, nickname, language } = req.body
+  fastify.post<{ Body: RegisterDto }>(
+    '/register',
+    {
+      schema: RegisterSchema,
+    },
+    async (req, reply) => {
+      const { email, password, nickname, language } = req.body
 
-    const { user, accessToken } = await prisma.$transaction(async (tx) => {
-      const existingUser = await tx.user.findUnique({
+      const { user, accessToken } = await prisma.$transaction(async (tx) => {
+        const existingUser = await tx.user.findUnique({
+          where: { email },
+        })
+
+        if (existingUser) {
+          throw getAPIError('EMAIL_ALREADY_EXIST')
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 10)
+
+        const user = await tx.user.create({
+          data: {
+            email,
+            password: hashedPassword,
+            nickname,
+            language,
+          },
+        })
+        const { accessToken, refreshToken } = await tokenService.generateTokens(
+          user.id,
+        )
+
+        reply.setCookie('refreshToken', refreshToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: 'lax',
+          path: '/auth/refresh',
+          signed: true,
+          maxAge: 7 * 24 * 60 * 60, // 7 days
+        })
+
+        return { user, accessToken }
+      })
+
+      return {
+        accessToken,
+        user: { ...user, password: undefined },
+      }
+    },
+  )
+
+  fastify.post<{ Body: LoginDto }>(
+    '/login',
+    { schema: LoginSchema },
+    async (req, reply) => {
+      const { email, password } = req.body
+
+      const user = await prisma.user.findUnique({
         where: { email },
       })
 
-      if (existingUser) {
-        throw getAPIError('EMAIL_ALREADY_EXIST')
+      if (!user) {
+        throw getAPIError('UNAUTHORIZED')
       }
 
-      const hashedPassword = await bcryptjs.hash(password, 10)
+      const isValid = await bcryptjs.compare(password, user.password)
 
-      const user = await tx.user.create({
-        data: {
-          email,
-          password: hashedPassword,
-          nickname,
-          language,
-        },
-      })
+      if (!isValid) {
+        throw getAPIError('UNAUTHORIZED')
+      }
+
       const { accessToken, refreshToken } = await tokenService.generateTokens(
         user.id,
       )
@@ -54,50 +97,12 @@ export default async function authRouter(fastify: FastifyInstance) {
         maxAge: 7 * 24 * 60 * 60, // 7 days
       })
 
-      return { user, accessToken }
-    })
-
-    return {
-      accessToken,
-      user: { ...user, password: undefined },
-    }
-  })
-
-  fastify.post<{ Body: LoginBody }>('/login', async (req, reply) => {
-    const { email, password } = req.body
-
-    const user = await prisma.user.findUnique({
-      where: { email },
-    })
-
-    if (!user) {
-      throw getAPIError('UNAUTHORIZED')
-    }
-
-    const isValid = await bcryptjs.compare(password, user.password)
-
-    if (!isValid) {
-      throw getAPIError('UNAUTHORIZED')
-    }
-
-    const { accessToken, refreshToken } = await tokenService.generateTokens(
-      user.id,
-    )
-
-    reply.setCookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      path: '/auth/refresh',
-      signed: true,
-      maxAge: 7 * 24 * 60 * 60, // 7 days
-    })
-
-    return {
-      accessToken,
-      user: { ...user, password: undefined },
-    }
-  })
+      return {
+        accessToken,
+        user: { ...user, password: undefined },
+      }
+    },
+  )
 
   fastify.post('/refresh', async (req, reply) => {
     const refreshToken = req.cookies.refreshToken
