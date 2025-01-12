0ebb94bb4f9446dfe7da543ad212a76fee1d9259
GWjun <kb865900@gmail.com>
2025-01-12 18:01:48 +0900

    test: Home page test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
+import { defineConfig as defineVitestConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
+const viteConfig = defineConfig({
   plugins: [react(), tsconfigPaths()],
 })
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      exclude: ['./server/**'],
+    },
+  },
+})
+
+export default mergeConfig(viteConfig, vitestConfig)
---
+++ vitest.setup.ts
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom/vitest'
+import { cleanup } from '@testing-library/react'
+import { afterEach, vi } from 'vitest'
+
+afterEach(() => {
+  cleanup()
+})
+
+vi.mock('zustand')
+
+vi.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (i18nKey: string) => i18nKey,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    }
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}))
